40aab0b7b55047b2792f58dca801a0dee4ff4bf9
umar <amar.m.sheikh@gmail.com>
2022-06-28 18:50:04 +0500

    add users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_28_134708) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_28_134939) do
   create_table "books", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -32,6 +32,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_28_134708) do
     t.datetime "due_at"
     t.boolean "deleted_at"
     t.integer "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
